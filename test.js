a6ad2c97e872edd2e9ee77fcccf8b761b2e0636e
尹梦淞 <1045349215@qq.com>
2023-03-24 10:12:49 +0800

    1

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
 let arr = [1,2,5,6,6]
 let res = arr[arr.length-1]
-console.log(res);
+// console.log(res);
 
 // console.log( new Date() instanceof Object );
 // console.log( typeof new Array());
-console.log( typeof new Function() );
+// console.log( typeof new Function() );
+let str = 'yms'
+// console.log(str.toString());
+{
+    let str = 'n_i ha+ohh-h'
+    let res =  str.split(/[_+-]|\s/)
+    console.log(res);
+}
